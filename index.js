430560e188d72ed73d96faffc790c5de08d02215
Jordan Bishop <bishopj1292@gmail.com>
2017-09-15 21:30:20 -0500

    Implement email address validation helper function

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
-function checkPhoneNumber(string) {
+function checkPhone(string) {
   var number = string.split('').filter((char) => !isNaN(parseInt(char)));
   if (number.length < 10 || number.length > 10) {
     alert('Please enter a valid 10-digit phone number.');
   } else {
     return number.join('');
+  }
+}
+
+function checkEmail(string) {
+  var atIndex = string.indexOf('@');
+  var dotIndex = string.indexOf('.');
+  if (atIndex > dotIndex || atIndex < 1 || dotIndex < 0 || dotIndex > string.length - 2) {
+    alert('Please enter a valid email address.');
+  } else {
+    return string;
   }
 }
