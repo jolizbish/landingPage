d9e2eae2a7dd6a15851bf2075cabccafe37d7759
Jordan Bishop <bishopj1292@gmail.com>
2017-09-15 21:22:19 -0500

    Implement phone number validation helper function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function checkPhoneNumber(string) {
+  var number = string.split('').filter((char) => !isNaN(parseInt(char)));
+  if (number.length < 10 || number.length > 10) {
+    alert('Please enter a valid 10-digit phone number.');
+  } else {
+    return number.join('');
+  }
+}
