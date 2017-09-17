8ab92c667965f2bc1e131781e563b97c29912ed2
Jordan Bishop <bishopj1292@gmail.com>
2017-09-16 20:10:59 -0500

    Implement jQuery selectors for form data & remove unnecessary html attributes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var $ = require("jquery");
 
 function checkPhone(string) {
   var number = string.split('').filter((char) => !isNaN(parseInt(char)));
@@ -31,6 +32,8 @@ function getLastName(string) {
   return string.split(' ')[1];
 }
 
+
+
 $(document).ready(function(e) {
 
   $("form").submit(function(e) {
@@ -39,33 +42,34 @@ $(document).ready(function(e) {
 
       var formData;
 
-      var first = getFirstName(this['0']['value']);
-      var last = getLastName(this['0']['value']);
-      var phone = checkPhone(this['1']['value']);
-      var email = checkEmail(this['2']['value']);
+      var first = getFirstName($('#name').val());
+      var last = getLastName($('#name').val());
+      var phone = checkPhone($('#phone').val());
+      var email = checkEmail($('#email').val());
 
       if (first && last && phone && email) {
-        formData = JSON.stringify({
+        formData = {
           FormVO:{
             firstName: first,
             lastName: last,
             cellPhone: phone,
             email: email
           }
-        });
+        };
 
         console.log('formData: ', formData);
 
         $.ajax({
           url: 'https://formsws-hilstaging-com-k0mfqqpppw0s.runscope.net/solar',
           type: 'POST',
+          dataType: 'json',
           data: formData,
           success: function(response){
-            console.log('successful response: ', response);
+            console.log('response: ', response);
           }
         });
       } else {
-        console.log('incomplete form');
+        console.log('incomplete form')
       }
   });
-})
+});
